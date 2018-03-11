0e80cf719b5e95b3ca48c6a56d852b7c97bfdfda
Linda Dick <linda.j.dick@gmail.com>
2018-03-11 14:16:07 -0500

    Improve disco startup scripts to check for dependencies rather than just sleeping

--- disco/start-disco-client.sh
+++ disco/start-disco-client.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-sleep 30
+SERVER="Disco Master"
+CLIENT="Disco Player"
+
+echo "Waiting for $SERVER to start..."
+
+while [ -z "$(pgrep "$SERVER" "$@")" ] ; do
+    sleep 1
+done
+
+echo "$SERVER started with process ID: " $(pgrep "$SERVER" "$@")
 
 ${HOME}/disco/disco --start
 
-sleep 4
+echo "Waiting for $CLIENT to start..."
 
-/usr/local/bin/darkice &
+while [ -z "$(pgrep "$CLIENT" "$@")" ] ; do
+    sleep 1
+done
+
+echo "$CLIENT started with process ID: " $(pgrep "$CLIENT" "$@")
+
+darkice &
 
 /usr/bin/python3 ${HOME}/disco/disco_update_icecast2.py
--- disco/start-disco-server.sh
+++ disco/start-disco-server.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+PULSE="pulseaudio"
+
 /usr/bin/pulseaudio --start
 
-sleep 10
+echo "Waiting for $PULSE to start..."
+
+while [ -z "$(pgrep "$PULSE" "$@")" ] ; do
+    sleep 1
+done
+
+echo "$PULSE started with process ID: " $(pgrep "$PULSE" "$@")
 
 ${HOME}/disco/discoserver
